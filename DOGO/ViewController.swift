6fc32a60562712a03c6eadd0071b360d7035ff47
Alam Figueroa <alam.figueroa@sjsu.edu>
2019-07-29 22:56:18 -0700

    Initial Commit

---
+++ DOGO/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DOGO
+//
+//  Created by Alam Figueroa Aguilar on 7/29/19.
+//  Copyright © 2019 HappyMonkeys. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
