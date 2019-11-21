1ea64187634064afae5cce2a79f18c6def771803
michinat <37168439+michinat@users.noreply.github.com>
2019-11-20 17:20:44 -0800

    dropdown button

--- DOGGO-redo/DOGGO-redo/Settings.swift
+++ DOGGO-redo/DOGGO-redo/Settings.swift
@@ -8,8 +8,9 @@
 
 import UIKit
 
-class Settings: UIViewController {
+class Settings: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    let array = ["Immediate", "Intermediate", "Far"]
     @IBAction func LEDSwitch(_ sender: UISwitch) {
         if(sender.isOn)
         {
@@ -32,7 +33,12 @@ class Settings: UIViewController {
         }
     }
     
-    @IBOutlet weak var rangeTextField: UITextField!
+    @IBOutlet weak var rangeButton: UIButton!
+    @IBOutlet weak var rangeTable: UITableView!
+    
+    @IBAction func rangeButtonPressed(_ sender: UIButton) {
+        self.rangeTable.isHidden = !self.rangeTable.isHidden
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -40,6 +46,25 @@ class Settings: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return array.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell1 = tableView.dequeueReusableCell(withIdentifier: "cell1", for: indexPath)
+        cell1.textLabel?.text = array[indexPath.row]
+        return cell1
+    }
+    
+    func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
+        let cell1 = tableView.cellForRow(at: indexPath)
+        rangeButton.setTitle(cell1?.textLabel?.text, for: .normal)
+        self.rangeTable.isHidden = true
+    }
 
     /*
     // MARK: - Navigation
